bce69b4a23e2416ff156462fb3170ff118d3672d
iurisilva05 <iurymito810@gmail.com>
2024-02-29 00:23:29 -0300

    header concertado

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,23 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HomComponent } from '../hom/hom.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage, HomComponent
   ], //facilita o carregamento da imagens
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  constructor(private router: Router) {}
+
+  navegarParaPaginadelogin() {
+    this.router.navigate(['/hom.component']);
+  }
 
 }
